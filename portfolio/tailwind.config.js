efc504670fe286ec1ef9d9ff5226fbb23d3e6472
EKTA MAURYA <mauryae440@gmail.com>
2024-10-14 23:39:20 +0530

    Add files via upload

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme:{
+            gridTemplateCOlums:{
+                'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            fontFamily: {
+                Outfit: ["Outfit", "sans-serif"], 
+                Ovo: ["Ovo", "serif"]
+            },
+            animation:{
+                spin_slow: 'spin 6s linear infinite'
+            },   
+        }
+    }
+        
